ae9ee58b1651c56089e53227af27510bea61b0e6
tusharkanti647 <107229334+tusharkanti647@users.noreply.github.com>
2023-01-11 03:07:20 +0000

    done

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,16 @@ class App extends Component {
 
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+               {/* Do not remove the main div */
+                 <ul key="relativeList">
+                    <li key="relativeListItem1">ram</li>
+                    <li key="relativeListItem2">sam</li>
+                    <li key="relativeListItem3">gita</li>
+                    <li key="relativeListItem4">amin</li>
+                    <li key="relativeListItem5">rina</li>
+                    <li key="relativeListItem6">rima</li>
+                 </ul>
+               }
             </div>
         )
     }
